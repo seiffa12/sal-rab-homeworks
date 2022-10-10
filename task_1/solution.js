800d67103715280382e30fbba73929e76e09f29b
seiffa12 <sergei198855@gmail.com>
2022-10-10 13:42:58 +0300

    task i done

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,7 +3,7 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-
+	// dwda
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
